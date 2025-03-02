d4f1ad2d6ccb0b542e16ea18362af761737be25c
Maria <mariakamboh55@gmail.com>
2025-03-02 13:40:57 +0500

    First release of my project

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import './Dashboard.css';
+
+function Dashboard(){
+    return(
+        <>
+        <div className="grid-container">
+        <img src="layout2.png" className="layout1" />
+        <img src="Dashboard1.png" className="dashboard" />
+        <img src="layout2.png" className="layout2" />
+
+        </div>
+        </>
+    );
+}
+const root=createRoot(document.getElementById('root'));
+root.render(<Dashboard />);
